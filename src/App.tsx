a3308fb70d903835c27605637ebfacb89bb60cf9
Michal <edii.poz@gmail.com>
2018-10-07 22:57:50 +0200

    add form logic, add callback function (form -> app)

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,25 @@ import './App.css';
 import Form from './components/Form/Form';
 import Picture from './components/Picture/Picture';
 
-
 class App extends React.Component {
   public render() {
     return (
       <div>
-        <Form/>
+        <Form formSubmitted={this.formSubmitted}/>
         <Picture/>
         <Picture/>
       </div>
     );
+  }
+
+  private async formSubmitted(data: any) {
+    alert("form submitted selected:" + data.selectedPet);
+    try {
+      const response = await fetch("http://shibe.online/api/shibes?count=1");
+      alert(response);
+    } catch (ex){
+      alert(ex);
+    }
   }
 }
 
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,71 @@
 import * as React from 'react';
 import './Form.css';
 
-class Form extends React.Component {
+interface IFormState {
+  value: number;
+  minValue: number;
+  maxValue: number;
+  pets: string[];
+  selectedPet: string;
+}
+
+interface IFormProps {
+  formSubmitted: (data: any) => void;
+}
+
+class Form extends React.Component<IFormProps, IFormState> {
+  constructor(props: IFormProps){
+    super(props);
+    this.state = { 
+      maxValue: 10,
+      minValue: 1, 
+      pets: [
+        "shibes",
+        "cats",
+        "birds",
+        "random" ],
+      selectedPet: "shibes",
+      value: 1 };
+    this.numberOnChange = this.numberOnChange.bind(this);
+    this.formOnSubmit = this.formOnSubmit.bind(this);
+    this.petsOnChange = this.petsOnChange.bind(this);
+  }
+
   public render() {
     return (
-      <div/>
+      <form onSubmit={this.formOnSubmit}>
+        <input type="number" 
+               min={this.state.minValue}
+               max={this.state.maxValue}
+               value={this.state.value}
+               onChange={this.numberOnChange}/>
+        <select onChange={this.petsOnChange}
+                defaultValue={this.state.selectedPet}>
+                {this.state.pets.map(pet => <option key={pet}>{pet}</option>)}
+        </select>
+        <button type="submit">Show</button>
+      </form>
     );
+  }
+
+  private numberOnChange(event: React.ChangeEvent<HTMLInputElement>){    
+    let newValue = parseInt(event.target.value, 10);
+    if (isNaN(newValue) ||
+        newValue < 1) {
+      newValue = this.state.minValue;
+    } else if (newValue > 10){
+      newValue = this.state.maxValue;
+    }
+    this.setState({ value: newValue });
+  }
+
+  private petsOnChange(event: React.ChangeEvent<HTMLSelectElement>){
+    this.setState({selectedPet: event.target.value});
+  }
+
+  private formOnSubmit(event: React.FormEvent<HTMLFormElement>){
+    this.props.formSubmitted({selectedPet: this.state.selectedPet});
+    event.preventDefault();
   }
 }
 
